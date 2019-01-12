a6edcc96d35b6bd1c34df2192843f6953408677a
birat21sk <birat.sk21@gmail.com>
2019-01-12 16:17:42 +0545

    BTY

---
+++ head.inc.php
@@ -0,0 +1,18 @@
+	<meta charset="utf-8">
+	<meta name="author" content="Scripters">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+	<title>By The Way</title>
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="dist/css/custom.css">
+	<link href="https://fonts.googleapis.com/css?family=Charm|Lobster|K2D|Pacifico|Permanent+Marker|Satisfy" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="dist/css/animate.css">
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+	<script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.js'></script>
+    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css' rel='stylesheet' />
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v3.1.3/mapbox-gl-directions.js'></script>
+	<link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v3.1.3/mapbox-gl-directions.css' type='text/css' />
+	<link rel="icon" type="png/img" href="images/icon.png">
